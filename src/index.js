0caf2030becea4eef1d1d5cd392a6d0afe133fa1
AtulPant2704 <atulpant1999@gmail.com>
2022-03-14 15:25:10 +0530

    feat: placed routes

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import App from "./App";
 import { makeServer } from "./server";
 import {BrowserRouter} from "react-router-dom"
+import App from "./App";
 
 // Call make Server
 makeServer();
