9f329ac0d6815c054b5e7f22a67eb9e407547bda
AtulPant2704 <atulpant1999@gmail.com>
2022-03-14 10:54:02 +0530

    feat: add login and signup pages

---
+++ src/pages/Authentication/SignUp.jsx
@@ -0,0 +1,61 @@
+import "./Authentication.css";
+
+const SignUp = () => {
+  return (
+    <section className="form-section">
+      <div className="form-wrapper">
+        <h2 className="form-heading">Signup</h2>
+        <form action="" method="post">
+          <div className="form-username">
+            <label for="name">Full Name</label>
+            <input
+              id="name"
+              type="text"
+              placeholder="Enter your name"
+              required
+            />
+          </div>
+          <div className="form-email">
+            <label for="email">Email address</label>
+            <input
+              id="email"
+              type="email"
+              placeholder="tanay@neog.camp"
+              required
+            />
+          </div>
+          <div className="form-password">
+            <label for="password">Password</label>
+            <input
+              id="password"
+              type="password"
+              placeholder="********"
+              required
+            />
+          </div>
+          <div className="form-confirm-password">
+            <label for="confirm-password">Confirm Password</label>
+            <input
+              id="confirm-password"
+              type="password"
+              placeholder="********"
+              required
+            />
+          </div>
+          <div className="user-history">
+            <input type="checkbox" id="user-request" />
+            <label for="user-request">I accept all Terms & Conditions</label>
+          </div>
+          <button type="submit" className="btn-submit">
+            Create New Account
+          </button>
+        </form>
+        <a href="../Login/Login" className="new-account">
+          Already have an Account <i className="fas fa-chevron-right"></i>
+        </a>
+      </div>
+    </section>
+  );
+};
+
+export { SignUp };
