76b1a6712c8001b12cd852364741656e1447d71b
AtulPant2704 <atulpant1999@gmail.com>
2022-03-14 10:44:18 +0530

    feat: add basic styling

--- src/App.js
+++ src/App.js
@@ -1,9 +1,19 @@
-import "./index.css";
+import "./App.css";
+import { Navbar } from "./components/Navbar";
+import { Footer } from "./components/Footer/Footer";
+import { Home } from "./pages/Homepage/Home";
+import { Login } from "./pages/Authentication/Login/Login";
+import { SignUp } from "./pages/Authentication/SignUp/SignUp";
+import { Wishlist } from "./pages/Wishlist/Wishlist";
+import { Cart } from "./pages/Cart/Cart";
+import { Products } from "./pages/Products/Products";
 
 function App() {
   return (
     <div className="App">
-      
+      <Navbar />
+      <Products />
+      <Footer />
     </div>
   );
 }
