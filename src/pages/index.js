22408b8d64f9043616b72bd0c652bab580fa4554
AtulPant2704 <atulpant1999@gmail.com>
2022-03-14 11:16:29 +0530

    refactor: exports in one file

---
+++ src/pages/index.js
@@ -0,0 +1,6 @@
+export {Login} from "./Authentication/Login";
+export {SignUp} from "./Authentication/SignUp";
+export {Cart} from "./Cart/Cart";
+export {Homepage} from "./Homepage/Home";
+export {Products} from "./Products/Products";
+export {Wishlist} from "./Wishlist/Wishlist";
