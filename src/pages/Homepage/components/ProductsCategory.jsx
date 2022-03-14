9c949bc4c9e24ce652add323cc11d3e85e5eba12
AtulPant2704 <atulpant1999@gmail.com>
2022-03-14 11:01:34 +0530

    feat: add landing page

---
+++ src/pages/Homepage/components/ProductsCategory.jsx
@@ -0,0 +1,15 @@
+const ProductsCategory = ({ cardType, cardImg, cardAlt, cardTitle }) => {
+    return (
+      <div className="card-container">
+        <div className="background">
+          <img src={cardImg} alt={cardAlt} className="img-responsive" />
+        </div>
+        <div className="content">
+          <h6 className="card-title">{cardTitle}</h6>
+        </div>
+      </div>
+    );
+  };
+  
+  export { ProductsCategory };
+  
