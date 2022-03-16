404fef63ea2fd28e549ad44d2f6a6d4b7cc69af2
AtulPant2704 <atulpant1999@gmail.com>
2022-03-16 17:50:32 +0530

    feat: exported all the filters

--- src/utils/index.js
+++ src/utils/index.js
@@ -1,2 +1,5 @@
-export { filterData } from "./filterData";
+export { categoryFilter } from "./categoryFilter";
 export { sortData } from "./sortData";
+export { priceFilter } from "./priceFilter";
+export { ratingFilter } from "./ratingFilter";
+export { inStockFilter } from "./inStockFilter";
