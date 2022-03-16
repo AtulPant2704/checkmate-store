68ea750cfb09e4099a241525fde084dec0793712
AtulPant2704 <atulpant1999@gmail.com>
2022-03-16 19:16:43 +0530

    refactor: removed extra variable

--- src/utils/sortData.js
+++ src/utils/sortData.js
@@ -1,14 +1,13 @@
 const sortData = (data, { sortBy }) => {
-    let tempData = [...data];
 
     if (sortBy === "LOW_TO_HIGH") {
-        tempData = tempData.sort((a, b) => a.price - b.price);
+        return [...data].sort((a, b) => a.price - b.price);
     }
     if (sortBy === "HIGH_TO_LOW") {
-        tempData = tempData.sort((a, b) => b.price - a.price);
+        return [...data].sort((a, b) => b.price - a.price);
     }
 
-    return tempData;
+    return data;
 };
 
 export { sortData };
