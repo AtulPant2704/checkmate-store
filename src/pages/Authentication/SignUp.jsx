324c7cb1eea872fef9214a80a555044005a9fb2a
AtulPant2704 <atulpant1999@gmail.com>
2022-03-18 13:37:26 +0530

    feat: function for signUp

--- src/pages/Authentication/SignUp.jsx
+++ src/pages/Authentication/SignUp.jsx
@@ -1,14 +1,18 @@
 import "./Authentication.css";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { useNavigate } from "react-router";
 import { useState } from "react";
-import { useAuth } from "../../hooks"
+import { useAuth } from "../../hooks";
 
 const SignUp = () => {
-  const { signUp } = useAuth();
+  const navigate = useNavigate();
+  const { authDispatch } = useAuth();
   const [user, setUser] = useState({
     email: "",
     password: "",
-    fullName: "",
+    firstName: "",
+    lastName: "",
     confirmPassword: ""
   })
 
@@ -17,12 +21,22 @@ const SignUp = () => {
     setUser({ ...user, [name]: value })
   }
 
-  const submitHandler = async (event) => {
-    console.log("signing up");
+  const signUpHandler = async (event) => {
     event.preventDefault();
     try {
-      let status = await signUp(user);
-      console.log("signed ");
+      const response = await axios.post("/api/auth/signup", user);
+      if (response.status === 201) {
+
+        localStorage.setItem("token", response.data.encodedToken);
+        localStorage.setItem("user", JSON.stringify(response.data.createdUser));
+
+        authDispatch({ type: "SIGN_UP", payload: { user: response.data.createdUser, token: response.data.encodedToken } })
+
+        navigate("/");
+      }
+      else {
+        throw new Error();
+      }
     }
     catch (error) {
       console.log(error);
@@ -35,50 +49,68 @@ const SignUp = () => {
         <h2 className="form-heading">Signup</h2>
         <form action="" method="post">
           <div className="form-username">
-            <label for="name">Full Name</label>
+            <label htmlFor="first-name">First Name</label>
             <input
-              id="name"
+              id="first-name"
               type="text"
               placeholder="Enter your name"
+              name="firstName"
+              value={user.firstName}
+              required
+              onChange={changeHandler}
+            />
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              id="last-name"
+              type="text"
+              placeholder="Enter your name"
+              name="lastName"
+              value={user.lastName}
               required
               onChange={changeHandler}
             />
           </div>
           <div className="form-email">
-            <label for="email">Email address</label>
+            <label htmlFor="email">Email address</label>
             <input
               id="email"
               type="email"
               placeholder="tanay@neog.camp"
+              name="email"
+              value={user.email}
               required
               onChange={changeHandler}
             />
           </div>
           <div className="form-password">
-            <label for="password">Password</label>
+            <label htmlFor="password">Password</label>
             <input
               id="password"
               type="password"
               placeholder="********"
+              name="password"
+              value={user.password}
               required
               onChange={changeHandler}
             />
           </div>
           <div className="form-confirm-password">
-            <label for="confirm-password">Confirm Password</label>
+            <label htmlFor="confirm-password">Confirm Password</label>
             <input
               id="confirm-password"
               type="password"
               placeholder="********"
+              name="confirmPassword"
+              value={user.confirmPassword}
               required
               onChange={changeHandler}
             />
           </div>
           <div className="user-history">
             <input type="checkbox" id="user-request" />
-            <label for="user-request">I accept all Terms & Conditions</label>
+            <label htmlFor="user-request">I accept all Terms & Conditions</label>
           </div>
-          <button type="submit" className="btn-submit" onClick={submitHandler}>
+          <button type="submit" className="btn-submit" onClick={signUpHandler}>
             Create New Account
           </button>
         </form>
@@ -86,7 +118,7 @@ const SignUp = () => {
           Already have an Account <i className="fas fa-chevron-right"></i>
         </Link>
       </div>
-    </section>
+    </section >
   );
 };
 
