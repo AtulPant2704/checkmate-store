160f78c52ad07d9bc6a32a9404efa9c4cf61e297
AtulPant2704 <atulpant1999@gmail.com>
2022-03-19 08:10:52 +0530

    feat: create cart context

---
+++ src/hooks/context/cart-context.jsx
@@ -0,0 +1,15 @@
+import { createContext, useContext } from "react";
+
+const CartContext = createContext(null);
+
+const CartProvider = ({ children }) => {
+    return (
+        <CartContext.Provider>
+            {children}
+        </CartContext.Provider>
+    )
+}
+
+const useCart = () => useContext(CartContext);
+
+export { CartProvider, useCart };
--- src/hooks/index.js
+++ src/hooks/index.js
@@ -1,2 +1,3 @@
 export { FilterProvider, useFilter } from "./context/filter-context";
 export { AuthProvider, useAuth } from "./context/auth-context";
+export { CartProvider, useCart } from "./context/cart-context";
