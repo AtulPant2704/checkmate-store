04a299b4fe2950de0d3687e13d212bfa33ca36c2
AtulPant2704 <atulpant1999@gmail.com>
2022-03-20 11:11:50 +0530

    feat: add cart actions

--- src/pages/Cart/components/CartItem.jsx
+++ src/pages/Cart/components/CartItem.jsx
@@ -1,26 +1,29 @@
-const CartItem = ({ cartImg, cartAlt, cartTitle, cartPrice }) => {
-    return (
-      <div className="cart-card">
-        <div className="img-container">
-          <img src={cartImg} alt={cartAlt} className="img-responsive" />
+const CartItem = ({ cartId, cartImg, cartAlt, cartTitle, cartPrice, cartQuantity, callRemoveFromCartHandler, callUpdateCartHandler }) => {
+  return (
+    <div className="cart-card">
+      <div className="img-container">
+        <img src={cartImg} alt={cartAlt} className="img-responsive" />
+      </div>
+      <div className="card-details">
+        <p className="card-title">{cartTitle}</p>
+        <h3 className="card-price">₹ {cartPrice}</h3>
+        <div className="quantity-controls">
+          <button className="btn-control decrease-btn" onClick={() => cartQuantity === 1 ? callRemoveFromCartHandler(cartId) : callUpdateCartHandler(cartId, "decrement")}>
+            <i className={`fas ${cartQuantity === 1 ? "fa-trash-alt" : "fa-minus"}`}></i>
+          </button>
+
+          <p className="quantity">{cartQuantity}</p>
+          <button className="btn-control increase-btn" onClick={() => callUpdateCartHandler(cartId, "increment")}>
+            <i className="fas fa-plus"></i>
+          </button>
         </div>
-        <div className="card-details">
-          <p className="card-title">{cartTitle}</p>
-          <h3 className="card-price">{cartPrice}</h3>
-          <div className="quantity-controls">
-            <button className="btn-control decrease-btn">
-              <i className="fas fa-minus"></i>
-            </button>
-            <p className="quantity">2</p>
-            <button className="btn-control increase-btn">
-              <i className="fas fa-plus"></i>
-            </button>
-          </div>
+        <div className="action-btns">
           <button className="wishlist-btn ecommerce-btn">Move to Wishlist</button>
+          <button className="wishlist-btn ecommerce-btn cart-remove-btn" onClick={() => callRemoveFromCartHandler(cartId)}>Remove</button>
         </div>
       </div>
-    );
-  };
-  
-  export { CartItem };
-  
+    </div>
+  );
+};
+
+export { CartItem };
