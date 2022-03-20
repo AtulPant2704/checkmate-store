d88029cad6030c36e6dca62c48560e90c734fc9a
AtulPant2704 <atulpant1999@gmail.com>
2022-03-20 20:17:03 +0530

    feat: add to wishlist

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,10 @@
+export { loginService } from "./authServices/login.service";
+export { signUpService } from "./authServices/signUp.service";
 export { getProductsService } from "./productServices/getProducts.service";
 export { getCartItemsService } from "./cartServices/getCartItems.service";
 export { addToCartService } from "./cartServices/addToCart.service";
 export { removeFromCartService } from "./cartServices/removeFromCart.service";
 export { updateCartService } from "./cartServices/updateCart.service";
-export { loginService } from "./authServices/login.service";
-export { signUpService } from "./authServices/signUp.service";
+export { getWishlistItemsService } from "./wishlistServices/getWishlistItems.service";
+export { addToWishlistService } from "./wishlistServices/addToWishlist.service";
+export { removeFromWishlistService } from "./wishlistServices/removeFromWishlist.service";
