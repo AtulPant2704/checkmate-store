57a5ae13f2c372a02666cd2be28ccec88a03ba76
AtulPant2704 <atulpant1999@gmail.com>
2022-03-21 15:51:54 +0530

    refactor: indentation

--- src/utils/wishlistHandlers/addToWishlistHandler.js
+++ src/utils/wishlistHandlers/addToWishlistHandler.js
@@ -4,7 +4,7 @@ const addToWishlistHandler = async (product, wishlistDispatch, token) => {
     try {
         const response = await addToWishlistService(product, token);
         if (response.status === 201) {
-            wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: response.data.wishlist })
+            wishlistDispatch({ type: "ADD_TO_WISHLIST", payload: response.data.wishlist });
         }
         else {
             throw new Error();
