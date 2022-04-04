5cd3ce3a924f630d71336f92f3b33148d2634117
AtulPant2704 <atulpant1999@gmail.com>
2022-04-04 22:22:24 +0530

    feat: add toast for wishlist handlers

--- src/utils/wishlistHandlers/moveToCartHandler.js
+++ src/utils/wishlistHandlers/moveToCartHandler.js
@@ -1,14 +1,25 @@
+import { toast } from "react-toastify";
 import { addToCartHandler } from "../cartHandlers/addToCartHandler";
 import { updateCartHandler } from "../cartHandlers/updateCartHandler";
 
-const moveToCartHandler = (_id, product, token, cartState, cartDispatch,setCartButtonLoader) => {
-    const { cart } = cartState;
-    const item = cart.find(item => item._id === _id);
-    if (item) {
-        updateCartHandler(_id, "increment", token, cartDispatch);
-    } else {
-        addToCartHandler(product, cartDispatch, token,setCartButtonLoader);
-    }
-}
+const moveToCartHandler = (
+  _id,
+  product,
+  token,
+  cartState,
+  cartDispatch,
+  setCartButtonLoader
+) => {
+  const { cart } = cartState;
+  const item = cart.find((item) => item._id === _id);
+  if (item) {
+    updateCartHandler(_id, "increment", token, cartDispatch);
+    toast.info(
+      `${product.title} already exists in cart, increased it's quantity`
+    );
+  } else {
+    addToCartHandler(product, cartDispatch, token, setCartButtonLoader);
+  }
+};
 
 export { moveToCartHandler };
