9a5f55685132a9389e2076e78f3e6937d70a67e2
AtulPant2704 <atulpant1999@gmail.com>
2022-04-15 11:01:44 +0530

    feat: add new address from address modal

--- src/pages/Profile/components/Address/AddressModal.jsx
+++ src/pages/Profile/components/Address/AddressModal.jsx
@@ -1,6 +1,49 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { toast } from "react-toastify";
+import { useAuth } from "../../../../context";
+import { addNewAddressHandler } from "../../../../utils";
 import "./AddressModal.css";
 
 const AddressModal = ({ showAddressModal, setShowAddressModal }) => {
+    const navigate = useNavigate();
+    const [address, setAddress] = useState({
+        name: "",
+        street: "",
+        city: "",
+        state: "",
+        country: "",
+        zipCode: "",
+        mobile: "",
+    })
+    const { authState: { token }, authDispatch } = useAuth();
+
+    const dummyAddress = {
+        name: "Guest User",
+        street: "House No. 111, MG Road, Indiranagar",
+        city: "Bangalore",
+        state: "Karnataka",
+        country: "India",
+        zipCode: "245789",
+        mobile: "9814235478",
+    };
+
+    const changeHandler = (event) => {
+        const { name, value } = event.target;
+        setAddress({ ...address, [name]: value });
+    };
+
+    const callAddNewAddressHandler = () => {
+        if (token) {
+            addNewAddressHandler(address, authDispatch, token);
+            setShowAddressModal(false);
+        }
+        else {
+            navigate("/login");
+            toast.warning("You're not logged in");
+        }
+    }
+
     return (
         <>
             {showAddressModal ?
@@ -10,19 +53,79 @@ const AddressModal = ({ showAddressModal, setShowAddressModal }) => {
                         <div class="form-wrapper address-form">
                             <h2 class="form-heading">Add New Address</h2>
                             <form>
-                                <input type="text" placeholder="Enter your name" required />
-                                <input type="text" placeholder="Enter house no, street or colony" required />
-                                <input type="text" placeholder="Enter City" required />
-                                <input type="text" placeholder="Enter State" required />
-                                <input type="text" placeholder="Enter Country" required />
-                                <input type="text" placeholder="Enter Zip Code" required />
-                                <input type="text" placeholder="Enter Mobile Number" required />
-                                <input type="text" placeholder="India" required />
+                                <input
+                                    type="text"
+                                    name="name"
+                                    value={address.name}
+                                    onChange={changeHandler}
+                                    placeholder="Enter your name"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="street"
+                                    value={address.street}
+                                    onChange={changeHandler}
+                                    placeholder="Enter house no, street or colony"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="city"
+                                    value={address.city}
+                                    onChange={changeHandler}
+                                    placeholder="Enter City"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="state"
+                                    value={address.state}
+                                    onChange={changeHandler}
+                                    placeholder="Enter State"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="country"
+                                    value={address.country}
+                                    onChange={changeHandler}
+                                    placeholder="Enter Country"
+                                    required
+                                />
+                                <input
+                                    type="text"
+                                    name="zipCode"
+                                    value={address.zipCode}
+                                    onChange={changeHandler}
+                                    placeholder="Enter Zip Code"
+                                    required
+                                />
+                                <input
+                                    type="number"
+                                    name="mobile"
+                                    value={address.mobile}
+                                    onChange={changeHandler}
+                                    placeholder="Enter Mobile Number"
+                                    required
+                                />
                             </form>
                             <div className="form-action-btns">
-                                <button className="btn btn-solid-primary action-btn save-btn">Save</button>
-                                <button className="btn btn-outline-primary action-btn dummy-btn">Dummy Address</button>
-                                <button className="btn btn-outline-primary action-btn cancel-btn" onClick={() => setShowAddressModal(false)}>Cancel</button>
+                                <button
+                                    className="btn btn-solid-primary action-btn save-btn"
+                                    onClick={callAddNewAddressHandler}>
+                                    Save
+                                </button>
+                                <button
+                                    className="btn btn-outline-primary action-btn dummy-btn"
+                                    onClick={() => setAddress(dummyAddress)}>
+                                    Dummy Address
+                                </button>
+                                <button
+                                    className="btn btn-outline-primary action-btn cancel-btn"
+                                    onClick={() => setShowAddressModal(false)}>
+                                    Cancel
+                                </button>
                             </div>
                         </div>
                     </div>
