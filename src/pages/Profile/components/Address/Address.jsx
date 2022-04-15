b480de7220159aa1a29e786d2aea747005216ee7
AtulPant2704 <atulpant1999@gmail.com>
2022-04-15 11:00:39 +0530

    feat: display user addresses

--- src/pages/Profile/components/Address/Address.jsx
+++ src/pages/Profile/components/Address/Address.jsx
@@ -1,22 +1,25 @@
-import { useState } from "react";
-import { AddressModal } from "./AddressModal";
+import { useAuth } from "../../../../context";
 import "./Address.css";
 
 const Address = ({ setShowAddressModal }) => {
+    const { authState: { addresses } } = useAuth();
+
     return (
         <>
             <div className="address-container">
-                <div className="address">
-                    <h3>Guest User</h3>
-                    <p>House No. 111, MG Road, Indiranagar</p>
-                    <p>Bangalore, Karnataka, 245789</p>
-                    <p>India</p>
-                    <p>Mobile: 9814235478</p>
-                    <div className="address-action-btns">
-                        <button className="btn btn-solid-primary btn-edit" onClick={() => setShowAddressModal(true)}>Edit</button>
-                        <button className="btn btn-outline-primary btn-remove">Remove</button>
+                {addresses.map(item => (
+                    <div className="address" key={item._id}>
+                        <h3>{item.name}</h3>
+                        <p>{item.street}</p>
+                        <p>{item.city}, {item.state}, {item.zipCode}</p>
+                        <p>{item.country}</p>
+                        <p>Mobile: {item.mobile}</p>
+                        <div className="address-action-btns">
+                            <button className="btn btn-solid-primary btn-edit" onClick={() => setShowAddressModal(true)}>Edit</button>
+                            <button className="btn btn-outline-primary btn-remove">Remove</button>
+                        </div>
                     </div>
-                </div>
+                ))}
                 <button
                     className="btn btn-text-primary new-address-btn"
                     onClick={() => setShowAddressModal(true)}>
