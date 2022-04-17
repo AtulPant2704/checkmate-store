25e77025752a7dce7d739d2d87c87a8bd9561757
AtulPant2704 <atulpant1999@gmail.com>
2022-04-17 21:41:28 +0530

    feat: display order products

--- src/pages/Profile/components/Orders/Orders.jsx
+++ src/pages/Profile/components/Orders/Orders.jsx
@@ -1,8 +1,63 @@
+import { useEffect } from "react";
+import { useAuth } from "../../../../context";
+import { getOrdersHandler } from "../../../../utils";
 import "./Orders.css"
 
 const Orders = () => {
+    const { authState: { token, orders }, authDispatch } = useAuth();
+
+    useEffect(() => getOrdersHandler(token, authDispatch), []);
+
+    console.log(orders);
     return (
-        <h1>Orders will be shown here</h1>
+        <>
+            {
+                orders.length > 0 ? orders.map(order => (
+                    <section className="cart-bill-container order-container" key={order._id}>
+                        <div className="bill-card">
+                            <h2 className="card-title">Order Confirmed</h2>
+                            <p className="order-date">{order.date}</p>
+                            <p className="order-id">Order Id:- #{order._id}</p>
+                            {order.orderedProducts.map(product => (
+                                <div className="cart-card" key={product._id}>
+                                    <div className="img-container">
+                                        <img src={product.image} alt={product.title} className="img-responsive" />
+                                    </div>
+                                    <div className="card-details">
+                                        <h3 className="card-title">{product.title}</h3>
+                                        <p className="card-price">₹ {product.price * product.qty}</p>
+                                        <p className="card-qty">Qty: {product.qty}</p>
+                                    </div>
+                                </div>
+                            ))}
+                            <div className="items-container delivery-address">
+                                <div className="items">
+                                    <h3 className="align-left">Delivery Address</h3>
+                                    <h3 className="address-title">{order.deliveryAddress.name}</h3>
+                                    <p>{order.deliveryAddress.street}, {order.deliveryAddress.city}, {order.deliveryAddress.state}, {order.deliveryAddress.country}, {order.deliveryAddress.zipCode}</p>
+                                    <p>Mobile: {order.deliveryAddress.mobile}</p>
+                                </div>
+                            </div>
+                            <div className="items-container">
+                                <h3 className="align-left">Price Details</h3>
+                                <div className="items-price">
+                                    <p className="item-type">Total MRP</p>
+                                    <p className="item-type-price">₹ {order.itemsPrice}</p>
+                                </div>
+                                <div className="items-price">
+                                    <p className="item-type">Total Discount</p>
+                                    <p className="item-type-price">₹ {order.discountedPrice}</p>
+                                </div>
+                                <div className="items-price total-price-container">
+                                    <p className="item-type total-price">Total Amount</p>
+                                    <p className="item-type-price total-price-value">₹ {order.totalPrice}</p>
+                                </div>
+                            </div>
+                        </div>
+                    </section >
+                ))
+                    : <h2>No orders to display</h2>}
+        </>
     )
 }
 
