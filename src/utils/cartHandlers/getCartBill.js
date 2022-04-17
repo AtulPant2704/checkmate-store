775d6e8b5ea6f9683f9ff63c70e8703176c60e9a
AtulPant2704 <atulpant1999@gmail.com>
2022-04-17 16:07:14 +0530

    refactor: get cart bill function

--- src/utils/cartHandlers/getCartBill.js
+++ src/utils/cartHandlers/getCartBill.js
@@ -1,11 +1,11 @@
-const getCartBill = (cart, couponType) => {
+const getCartBill = (cart, couponType = "") => {
   const cartQuantity = cart.reduce((acc, curr) => acc + Number(curr.qty), 0);
   const itemsPrice = cart.reduce(
     (acc, curr) => acc + Number(curr.price * curr.qty),
     0
   );
   let totalPrice = itemsPrice - 500;
-  if (couponType !== "") {
+  if (couponType) {
     totalPrice = totalPrice * (1 - Number(couponType) / 100);
   }
   return { cartQuantity, itemsPrice, totalPrice };
